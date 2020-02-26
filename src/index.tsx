37382dbe83c7eed437c73bba4832a72767a8decb
Tom Andrews <git@tomandrews.co.uk>
2020-02-26 22:00:57 +0000

    First version

---
+++ src/index.tsx
@@ -0,0 +1,32 @@
+import React from "react";
+
+const CONTAINER_ID = "commento";
+const COMMENTO_URL = "https://cdn.commento.io/js/commento.js";
+
+const Commento = ({
+  cssOverride,
+  autoInit,
+  noFonts,
+  hideDeleted,
+  pageId
+}: {
+  cssOverride?: string;
+  autoInit?: boolean;
+  noFonts?: boolean;
+  hideDeleted?: boolean;
+  pageId?: string;
+}) => (
+  <>
+    <div id={CONTAINER_ID} />
+    <script
+      src={COMMENTO_URL}
+      async={true}
+      data-css-override={cssOverride}
+      data-auto-init={autoInit}
+      data-no-fonts={noFonts}
+      data-hide-delete={hideDeleted}
+      data-page-id={pageId}
+    />
+  </>
+);
+export default Commento;
